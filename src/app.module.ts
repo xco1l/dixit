e69f69ed9153036aa5a0d6842ec131db48758561
Saniyook <saniyook@gmail.com>
2020-04-03 23:18:01 +0300

    moved root config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-
-const env = process.env.NODE_ENV || 'development';
+import { configModule } from './configure.root';
 
 @Module({
   imports: [
     UserModule,
     AuthModule,
-    ConfigModule.forRoot({
-      envFilePath: `.env.${env}`,
-      isGlobal: true,
-    }),
+    configModule,
     MongooseModule.forRoot(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
