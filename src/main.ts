375c67098280732bf098d3cbc5bfc535d5c90f02
Saniyook <saniyook@gmail.com>
2020-04-04 15:34:06 +0300

    added swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Dixit game API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
