aaffc9505ae51ad773d0ae608ee47c3db145a0b9
Saniyook <saniyook@gmail.com>
2020-04-11 15:54:31 +0300

    added global prefix for api

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api/v1');
   const options = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('Dixit game API')
